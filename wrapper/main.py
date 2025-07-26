216727e6ffe09924648fbe50a750affc619bfaea
averytruax <averytruax@outlook.com>
2025-07-26 02:28:35 -0700

    Renamed files / added init files for modules / transferred lots of functions to classes

---
+++ wrapper/main.py
@@ -0,0 +1,74 @@
+# Import Scripts to be used
+from std_atm import std_atm
+from geometry.geometric_functions import AirfoilFunctions
+from wrapper.runner_airfoil import airfoil_desc , optimization_parameters
+from xfoil_wrapper import XfoilWrapper
+import matplotlib.pyplot as plt
+from math import sqrt
+
+
+
+# #Name of the file for the final drag polar to be output to
+# datafile = 'wrapper_data.txt'
+
+#Options for the wrapper =========================================
+M = 0.2   #Mach number for the analysis (Keep below M = 0.7)
+altitude = 30000  #Altitude for the analysis (specify units as third argument
+
+#=================================================================
+
+if __name__ == "__main__":
+
+    # Create the airfoil and export it as a dat file
+    upper_coords = gf.cst_xy(airfoil_desc['cst_upper'],
+                             airfoil_desc['yte'],
+                             airfoil_desc['type'],
+                             num_points=50)
+    lower_coords = gf.cst_xy(airfoil_desc['cst_lower'],
+                             airfoil_desc['yte'],
+                             airfoil_desc['type'],
+                             num_points=50)
+
+    gf.write_dat_file('wrapper_airfoil.dat',upper_coords,lower_coords)
+
+    atm_data = std_atm(altitude,'ft')
+    a = sqrt(1.4 * 287 * atm_data['Temperature'])
+    Re = (atm_data['Density'] * M * a) / atm_data['Viscosity'] # 1/m
+    print(M*a)
+    print(atm_data['Density'])
+    print(atm_data['Temperature'])
+    print(atm_data['Viscosity'])
+    print(Re)
+
+    run = XfoilWrapper()
+
+    # run.test_this_thang(Re,M)
+
+    polar = run.get_airfoil_polar_data(Re,M)
+
+    # fig = plt.figure(figsize=(8,5))
+    # ax = plt.axes()
+    # ax.set_xlabel('Cd')
+    # ax.set_ylabel('Cl')
+    # ax.set_xlim((min(polar['cd'][:])-0.1*min(polar['cd'][:]),max(polar['cd'][:])+0.1*max(polar['cd'][:])))
+    # ax.set_ylim((min(polar['cl'][:])-0.1*min(polar['cl'][:]),max(polar['cl'][:])+0.1*max(polar['cl'][:])))
+    # plt.plot(polar['cd'],polar['cl'])
+    # plt.savefig('Drag_Polar.png')
+
+    # fig = plt.figure(figsize=(8,5))
+    # ax = plt.axes()
+    # ax.set_xlabel('alpha')
+    # ax.set_ylabel('Cl')
+    # ax.set_ylim((min(polar['cl'][:])-0.1*min(polar['cl'][:]),max(polar['cl'][:])+0.1*max(polar['cl'][:])))
+    # ax.set_xlim((min(polar['alpha'][:])-0.1*min(polar['alpha'][:]),max(polar['alpha'][:])+0.1*max(polar['alpha'][:])))
+    # plt.plot(polar['alpha'],polar['cl'])
+    # plt.savefig('Lift_Curve.png')
+
+    # fig = plt.figure(figsize=(8,5))
+    # ax = plt.axes()
+    # ax.set_xlabel('alpha')
+    # ax.set_ylabel('cd')
+    # ax.set_ylim((min(polar['cd'][:])-0.1*min(polar['cd'][:]),max(polar['cd'][:])+0.1*max(polar['cd'][:])))
+    # ax.set_xlim((min(polar['alpha'][:])-0.1*min(polar['alpha'][:]),max(polar['alpha'][:])+0.1*max(polar['alpha'][:])))
+    # plt.plot(polar['alpha'],polar['cd'])
+    # plt.savefig('Drag_Bucket.png')
---
+++ wrapper/runner_airfoil.py
@@ -0,0 +1,41 @@
+import numpy as np
+from pydantic import BaseModel
+
+"""Script containing the geometric information about the airfoils and the optimization parameters"""
+class RunnerAirfoil(BaseModel):
+    def __init__(self):
+
+        self.tag: str
+        self.airfoil_cst_coefficients: dict = None
+        self.settings: dict = None
+        self.optimization_parameters: dict = None
+
+        return
+
+
+    @classmethod
+    def test_airfoil(cls):
+
+        airfoil = cls()
+
+        airfoil.settings = "test_airfoil"
+
+        airfoil.airfoil_cst_coefficients = {
+            'cst_upper' : np.array([0.102220,0.15578,0.099851,0.069602,0.4,-0.15,1e-06,0.35]),
+            'cst_lower' : np.array([-0.22348,-0.28056,-0.11018,-0.13087,-0.28754,-0.25265,-0.14652,-0.35407]),
+
+        }
+
+        airfoil.airfoil_settings = {
+            'yte'       : 0.0,
+            'type'      : 'round',
+        }
+
+        # TODO: move these somewhere else in the repo
+        optimization_parameters = {
+            'thickness_chord_lim' : 0.12,
+            'geometric_iteration_lim' : 15.0,
+            'target_cl_max' : 1.2,
+            'target_cd0_' : 0.02,
+            'target_cm0' : 0.01,
+        }
